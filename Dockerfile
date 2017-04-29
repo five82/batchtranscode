e11332677562e8726d8ec8fd9e065310eed682e6
five82 <five82@users.noreply.github.com>
2017-04-29 17:15:48 -0400

    Adding container files.

---
+++ Dockerfile
@@ -0,0 +1,122 @@
+# Use Ubuntu 16.04 as a base image
+FROM ubuntu:16.04
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+ADD . /app
+
+# Update and install dependencies
+RUN \
+
+apt-get update && \
+apt-get install -y \
+  curl \
+  autoconf \
+  automake \
+  build-essential \
+  libass-dev \
+  libfreetype6-dev \
+  libsdl1.2-dev \
+  libtheora-dev \
+  libtool \
+  libva-dev \
+  libvdpau-dev \
+  libvorbis-dev \
+  libxcb1-dev \
+  libxcb-shm0-dev \
+  libxcb-xfixes0-dev \
+  pkg-config \
+  texinfo \
+  zlib1g-dev \
+  git mercurial \
+  cmake \
+  yasm && \
+
+# Setup directories
+mkdir -p /input /output /ffmpeg/ffmpeg_sources && \
+
+# Compile and install ffmpeg and ffprobe
+cd /ffmpeg/ffmpeg_sources && \
+git clone --depth=1 git://git.videolan.org/x264 && \
+hg clone https://bitbucket.org/multicoreware/x265 && \
+git clone --depth=1 https://github.com/FFmpeg/FFmpeg.git ffmpeg && \
+
+cd /ffmpeg/ffmpeg_sources/x264 && \
+PATH="/ffmpeg/bin:$PATH" ./configure --prefix="/ffmpeg/ffmpeg_build" --bindir="/ffmpeg/bin" --enable-pic --enable-shared --enable-static && \
+PATH="/ffmpeg/bin:$PATH" make && \
+make install && \
+make distclean && \
+
+cd /ffmpeg/ffmpeg_sources/x265/build/linux && \
+PATH="/ffmpeg/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="/ffmpeg/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source && \
+make && \
+make install && \
+
+cd /ffmpeg/ffmpeg_sources/ffmpeg && \
+PATH="/ffmpeg/bin:$PATH" PKG_CONFIG_PATH="/ffmpeg/ffmpeg_build/lib/pkgconfig" ./configure \
+--prefix="/ffmpeg/ffmpeg_build" \
+--extra-cflags="-I/ffmpeg/ffmpeg_build/include -static" \
+--extra-cflags=--static \
+--extra-ldflags="-L/ffmpeg/ffmpeg_build/lib -lm -static" \
+--extra-version=static \
+--bindir="/ffmpeg/bin" \
+--pkg-config-flags="--static" \
+--enable-static \
+--disable-shared \
+--disable-debug \
+--enable-ffprobe \
+--disable-ffserver \
+--enable-gpl \
+--enable-nonfree \
+--enable-libx264 \
+--enable-libx265 && \
+make && \
+make install && \
+make distclean && \
+hash -r && \
+
+# Copy ffmpeg and ffprobe to app directory
+cp /ffmpeg/bin/ff* /app/ && \
+
+# Clean up directories and packages after compilation
+rm -rf /ffmpeg && \
+apt-get remove -y \
+  autoconf \
+  automake \
+  build-essential \
+  libass-dev \
+  libfreetype6-dev \
+  libsdl1.2-dev \
+  libtheora-dev \
+  libtool \
+  libva-dev \
+  libvdpau-dev \
+  libvorbis-dev \
+  libxcb1-dev \
+  libxcb-shm0-dev \
+  libxcb-xfixes0-dev \
+  pkg-config \
+  texinfo \
+  zlib1g-dev \
+  git \
+  mercurial \
+  cmake \
+  yasm && \
+apt-get -y autoremove && \
+apt-get clean && \
+
+# Set transcode script as executable
+chmod +x /app/transcode.sh
+
+# Environment variables
+ENV encoder=x264 \
+    hdcrf=20 \
+    sdcrf=20 \
+    preset=veryfast \
+    cropscan=600 \
+    slackurl=https://127.0.0.1
+
+# Run transcode.sh when the container launches
+CMD ["/app/transcode.sh"]
---
+++ transcode.sh
@@ -0,0 +1,286 @@
+#!/bin/bash
+
+# ffmpeg batch transcode script
+# Author: five82
+# https://github.com/five82
+# Takes mkv files, analyzes, and batch transcodes automatically based on the following conditions:
+#   number of audio tracks
+#   number of channels in the first audio track. set bitrate accordingly.
+#   determines crf based of width of video - is it 1080p or lower?
+#   is there a forced subtitle track? if so, set it to forced.
+
+# LIMITATIONS:
+# limit of five audio tracks for input video
+# limit of one forced subtitle track for input video
+# stereo only output for second, third, fourth and fifth audio tracks (intended for commentary tracks)
+
+
+# INPUT AND OUTPUT DIRECTORIES:
+inputdir=/input
+outputdir=/output
+
+fun_slackpost () {
+  slackmsg="$1"
+  case "$2" in
+    INFO)
+      slackicon=':slack:'
+      ;;
+    WARNING)
+      slackicon=':warning:'
+      ;;
+    ERROR)
+      slackicon=':bangbang:'
+      ;;
+    *)
+      slackicon=':slack:'
+      ;;
+  esac
+  curl -X POST --data "payload={\"text\": \"${slackicon} ${slackmsg}\"}" ${slackurl}
+}
+
+# Transcoding function
+fun_transcode () {
+
+  # Analyze input video
+  tracks=$(/app/ffprobe -v error -show_entries format=nb_streams -of default=noprint_wrappers=1:nokey=1:noprint_wrappers=1 "${input}")
+  forced=$(/app/ffprobe -v error -show_entries stream=index -select_streams s -of default=noprint_wrappers=1:nokey=1:noprint_wrappers=1 "${input}")
+  channels=$(/app/ffprobe -v error -select_streams a:0 -show_entries stream=channels -of default=nokey=1:noprint_wrappers=1 "${input}")
+  inputvcodec=$(/app/ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 "${input}")
+  eval $(/app/ffprobe -v error -of flat=s=_ -select_streams v:0 -show_entries stream=width "${input}")
+  width=${streams_stream_0_width}
+  vidcrop=$(/app/ffmpeg -ss "${cropscan}" -i "${input}" -f matroska -t 120 -an -vf cropdetect=24:16:0 -y -crf 51 -preset ultrafast /dev/null 2>&1 | grep -o crop=.* | sort -bh | uniq -c | sort -bh | tail -n1 | grep -o crop=.*)
+
+  # Determine output file name
+  inputfilename=$(basename "${input}")
+  output="${outputdir}/${inputfilename}"
+
+  # Determine number of audio tracks
+  if [ ${forced} ]; then
+    atracks=$(( $tracks-2 ))
+  else
+    atracks=$(( $tracks-1 ))
+  fi
+
+  # Set crf based on video width
+  if (( ${width} < 1900 )); then
+    crf=${sdcrf}
+  else
+    crf=${hdcrf}
+  fi
+
+  # Set encoder variables
+  if [ ${encoder} == "x264" ]; then
+    encoderlib="libx264"
+    encoderparams="-x264-params"
+  elif [ ${encoder} == "x265" ]; then
+    encoderlib="libx265"
+    encoderparams="-x265-params"
+  else
+    echo "ERROR: Invalid encoder ${encoder} specified. Set the encoder to x264 or x265. Aborting."
+    exit
+  fi
+
+  # Set bitrate for first audio track based on the number of channels
+  if [ ${channels} == 2 ]; then
+    abitrate="128k"
+  elif [ ${channels} == 6 ]; then
+    abitrate="384k"
+  elif [ ${channels} == 8 ]; then
+    abitrate="512k"
+  else
+    echo "ERROR: Invalid channel number ${channels} in first audio track of ${inputfilename}. Aborting."
+    exit
+  fi
+
+  # Transcode
+  if [ ${atracks} == 1 ] && [ !  ${forced} ]; then
+    echo "inputfilename=$inputfilename"
+    echo "atracks=$atracks"
+    echo "forced=$forced"
+    echo "You have selected 1 audio track, no subtitles."
+    fun_slackpost "Starting encode: $inputfilename" "INFO"
+    /app/ffmpeg \
+      -i ${input} \
+      -vf ${vidcrop} \
+      -map 0:0 -map 0:1 \
+      -c:v ${encoderlib} -preset ${preset} ${encoderparams} crf=${crf} \
+      -c:a:0 aac -b:a:0 ${abitrate} \
+      ${output}
+    fun_slackpost "Finished encode: $inputfilename" "INFO"
+  elif [ ${atracks} == 1 ] && [ ${forced} ]; then
+    echo "inputfilename=$inputfilename"
+    echo "atracks=$atracks"
+    echo "forced=$forced"
+    echo "You have selected 1 audio track, subtitles."
+    fun_slackpost "Starting encode: $inputfilename" "INFO"
+    /app/ffmpeg \
+      -i ${input} -map 0:0 -map 0:1 -map 0:2 \
+      -vf ${vidcrop} \
+      -c:v ${encoderlib} -preset ${preset} ${encoderparams} crf=${crf} \
+      -c:a:0 aac -b:a:0 ${abitrate} \
+      -c:s:0 copy -disposition:s:0 +default+forced \
+      ${output}
+    fun_slackpost "Finished encode: $inputfilename" "INFO"
+  elif [ ${atracks} == 2 ] && [ ! ${forced} ]; then
+    echo "inputfilename=$inputfilename"
+    echo "atracks=$atracks"
+    echo "forced=$forced"
+    echo "You have selected 2 audio tracks, no subtitles."
+    fun_slackpost "Starting encode: $inputfilename" "INFO"
+    /app/ffmpeg \
+      -i ${input} \
+      -vf ${vidcrop} \
+      -map 0:0 -map 0:1 -map 0:2 \
+      -c:v ${encoderlib} -preset ${preset} ${encoderparams} crf=${crf} \
+      -c:a:0 aac -b:a:0 ${abitrate} \
+      -c:a:1 aac -b:a:1 128k \
+      ${output}
+    fun_slackpost "Finished encode: $inputfilename" "INFO"
+  elif [ ${atracks} == 2 ] && [ ${forced} ]; then
+    echo "inputfilename=$inputfilename"
+    echo "atracks=$atracks"
+    echo "forced=$forced"
+    echo "You have selected 2 audio tracks, subtitles."
+    fun_slackpost "Starting encode: $inputfilename" "INFO"
+    /app/ffmpeg \
+      -i ${input} \
+      -vf ${vidcrop} \
+      -map 0:0 -map 0:1 -map 0:2 -map 0:3 \
+      -c:v ${encoderlib} -preset ${preset} ${encoderparams} crf=${crf} \
+      -c:a:0 aac -b:a:0 ${abitrate} \
+      -c:a:1 aac -b:a:1 128k \
+      -c:s:0 copy -disposition:s:0 +default+forced \
+      ${output}
+    fun_slackpost "Finished encode: $inputfilename" "INFO"
+  elif [ ${atracks} == 3 ] && [ ! ${forced} ]; then
+    echo "inputfilename=$inputfilename"
+    echo "atracks=$atracks"
+    echo "forced=$forced"
+    echo "You have selected 3 audio tracks, no subtitles."
+    fun_slackpost "Starting encode: $inputfilename" "INFO"
+    /app/ffmpeg \
+      -i ${input} \
+      -vf ${vidcrop} \
+      -map 0:0 -map 0:1 -map 0:2 -map 0:3 \
+      -c:v ${encoderlib} -preset ${preset} ${encoderparams} crf=${crf} \
+      -c:a:0 aac -b:a:0 ${abitrate} \
+      -c:a:1 aac -b:a:1 128k \
+      -c:a:2 aac -b:a:2 128k \
+      ${output}
+    fun_slackpost "Finished encode: $inputfilename" "INFO"
+  elif [ ${atracks} == 3 ] && [ ${forced} ]; then
+    echo "inputfilename=$inputfilename"
+    echo "atracks=$atracks"
+    echo "forced=$forced"
+    echo "You have selected 3 audio tracks, subtitles."
+    fun_slackpost "Starting encode: $inputfilename" "INFO"
+    /app/ffmpeg \
+      -i ${input} \
+      -vf ${vidcrop} \
+      -map 0:0 -map 0:1 -map 0:2 -map 0:3 -map 0:4 \
+      -c:v ${encoderlib} -preset ${preset} ${encoderparams} crf=${crf} \
+      -c:a:0 aac -b:a:0 ${abitrate} \
+      -c:a:1 aac -b:a:1 128k \
+      -c:a:2 aac -b:a:2 128k \
+      -c:s:0 copy -disposition:s:0 +default+forced \
+      ${output}
+    fun_slackpost "Finished encode: $inputfilename" "INFO"
+  elif [ ${atracks} == 4 ] && [ ! ${forced} ]; then
+    echo "inputfilename=$inputfilename"
+    echo "atracks=$atracks"
+    echo "forced=$forced"
+    echo "You have selected 4 audio tracks, no subtitles."
+    fun_slackpost "Starting encode: $inputfilename" "INFO"
+    /app/ffmpeg \
+      -i ${input} \
+      -vf ${vidcrop} \
+      -map 0:0 -map 0:1 -map 0:2 -map 0:3 -map 0:4 \
+      -c:v ${encoderlib} -preset ${preset} ${encoderparams} crf=${crf} \
+      -c:a:0 aac -b:a:0 ${abitrate} \
+      -c:a:1 aac -b:a:1 128k \
+      -c:a:2 aac -b:a:2 128k \
+      -c:a:3 aac -b:a:3 128k \
+      ${output}
+    fun_slackpost "Finished encode: $inputfilename" "INFO"
+  elif [ ${atracks} == 4 ] && [ ${forced} ]; then
+    echo "inputfilename=$inputfilename"
+    echo "atracks=$atracks"
+    echo "forced=$forced"
+    echo "You have selected 4 audio tracks, subtitles."
+    fun_slackpost "Starting encode: $inputfilename" "INFO"
+    /app/ffmpeg \
+      -i ${input} \
+      -vf ${vidcrop} \
+      -map 0:0 -map 0:1 -map 0:2 -map 0:3 -map 0:4 -map 0:5 \
+      -c:v ${encoderlib} -preset ${preset} ${encoderparams} crf=${crf} \
+      -c:a:0 aac -b:a:0 ${abitrate} \
+      -c:a:1 aac -b:a:1 128k \
+      -c:a:2 aac -b:a:2 128k \
+      -c:a:3 aac -b:a:3 128k \
+      -c:s:0 copy -disposition:s:0 +default+forced \
+      ${output}
+    fun_slackpost "Finished encode: $inputfilename" "INFO"
+  elif [ ${atracks} == 5 ] && [ ! ${forced} ]; then
+    echo "inputfilename=$inputfilename"
+    echo "atracks=$atracks"
+    echo "forced=$forced"
+    echo "You have selected 5 audio tracks, no subtitles."
+    fun_slackpost "Starting encode: $inputfilename" "INFO"
+    /app/ffmpeg \
+      -i ${input} \
+      -vf ${vidcrop} \
+      -map 0:0 -map 0:1 -map 0:2 -map 0:3 -map 0:4 -map 0:5 \
+      -c:v ${encoderlib} -preset ${preset} ${encoderparams} crf=${crf} \
+      -c:a:0 aac -b:a:0 ${abitrate} \
+      -c:a:1 aac -b:a:1 128k \
+      -c:a:2 aac -b:a:2 128k \
+      -c:a:3 aac -b:a:3 128k \
+      -c:a:3 aac -b:a:4 128k \
+      ${output}
+    fun_slackpost "Finished encode: $inputfilename" "INFO"
+  elif [ ${atracks} == 5 ] && [ ${forced} ]; then
+    echo "inputfilename=$inputfilename"
+    echo "atracks=$atracks"
+    echo "forced=$forced"
+    echo "You have selected 5 audio tracks, subtitles."
+    fun_slackpost "Starting encode: $inputfilename" "INFO"
+    /app/ffmpeg \
+      -i ${input} \
+      -vf ${vidcrop} \
+      -map 0:0 -map 0:1 -map 0:2 -map 0:3 -map 0:4 -map 0:5 -map 0:6 \
+      -c:v ${encoderlib} -preset ${preset} ${encoderparams} crf=${crf} \
+      -c:a:0 aac -b:a:0 ${abitrate} \
+      -c:a:1 aac -b:a:1 128k \
+      -c:a:2 aac -b:a:2 128k \
+      -c:a:3 aac -b:a:3 128k \
+      -c:a:3 aac -b:a:4 128k \
+      -c:s:0 copy -disposition:s:0 +default+forced \
+      ${output}
+    fun_slackpost "Finished encode: $inputfilename" "INFO"
+  else
+    echo "ERROR: Invalid encoding parameters Aborting."
+    echo "inputfilename=$inputfilename"
+    echo "output=$output"
+    echo "tracks=$tracks"
+    echo "atracks=$atracks"
+    echo "abitrate=$abitrate"
+    echo "forced=$forced"
+    echo "channels=$channels"
+    echo "width=$width"
+    echo "crf=$crf"
+    exit
+  fi  
+}
+
+
+# Transcode each file in the input directory
+count=0
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+for input in ${inputdir}/*.mkv; do
+  count=$(( $count+1 ))
+  echo "INFO: Transcode $count. File: ${input}."
+  fun_transcode
+done
+fun_slackpost "Job complete." "INFO"
+IFS=$SAVEIFS
